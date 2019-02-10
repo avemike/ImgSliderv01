90763e95836e45f23b2cdd295c20c31c9776de4a
avemike <fajny.michal.gajek@gmail.com>
2019-02-10 23:38:36 +0100

    Updates

---
+++ precache-manifest.67350f13cdce6c013d04a1c58dd278f8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f033d03ec6d5dbf0e501",
+    "url": "/ImgSliderv01/static/css/main.eb12a1d0.chunk.css"
+  },
+  {
+    "revision": "f033d03ec6d5dbf0e501",
+    "url": "/ImgSliderv01/static/js/main.f033d03e.chunk.js"
+  },
+  {
+    "revision": "5b241c1cc36b485fa6a1",
+    "url": "/ImgSliderv01/static/js/1.5b241c1c.chunk.js"
+  },
+  {
+    "revision": "c3fb9bf017af6b19db42",
+    "url": "/ImgSliderv01/static/js/runtime~main.c3fb9bf0.js"
+  },
+  {
+    "revision": "53c936b6f4a5531b2f1b02a2528e8e0a",
+    "url": "/ImgSliderv01/index.html"
+  }
+];
